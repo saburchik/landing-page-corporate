8881d3854ab137866ba6b2f0868bb8cb03837736
Yury <the.saburchik@gmail.com>
2020-11-03 00:49:24 +0700

    add code

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// scroll until registration of button TRY NOW
+
+$(document).ready(function () {
+    $("#js-btn").on("click", function (e) {
+        e.preventDefault();
+        const top = $("#registration").offset().top;
+        $("html, body").animate({
+            scrollTop: top
+        }, 500);
+    });
+});
